ed8597ef56773a3651f2894bc618aa23f8f5d702
promel <jkm.more@gmail.com>
2021-12-30 17:25:56 +0200

    download issue fixed

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
+import os
 
-import ssl
+def checkAndCreateFolder(path):
 
-# This restores the same behavior as before.
-context = ssl._create_unverified_context()
+    # Check whether the specified path exists or not
+    isExist = os.path.exists(path)
+
+    if not isExist:
+        # Create a new directory because it does not exist 
+        os.makedirs(path)
+        print(f"The {path} directory is created!")
+
+def checkExtension(link):
+    for ext in ['.pdf','.docx','.zip']:
+        if ext in link:
+            return ext
 
 
-path = './html/results.html'
+htmlFolder = './html'
+checkAndCreateFolder(htmlFolder)
+path = f'{htmlFolder}/results.html'
 
 url = input('Please end link: ')
 request = requests.get(url)
@@ -18,16 +31,16 @@ f.write(request.content)
 soup = BeautifulSoup(open(path,encoding="utf8"),features="html.parser")
 hrefs = soup.select('p > a')
 
+filesFolder = './files'
+checkAndCreateFolder(filesFolder)
+
 for href in hrefs:
     title = href.text   
     link = href.get('href')
-    if('.pdf' in link or '.docx' in link):
-        ext =  'pdf' if '.pdf' in link else 'docx'
-        # urllib.request.urlretrieve(link,"./files/" + f'{title}.{ext}')
-
+    ext = checkExtension(link)
+    if(ext):
         r = requests.get(link, allow_redirects=True)
+        filename = f'{title}{ext}'
+        open(f'{filesFolder}/{filename}', 'wb').write(r.content)
 
-        open("./files/" + f'{title}.{ext}', 'wb').write(r.content)
-
-        print("downloaded : " + title )
-        break
+        print("downloaded : " + filename )
